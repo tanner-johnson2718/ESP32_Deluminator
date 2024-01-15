041a68480be538a5c6160163505c23de722f38ae
Tanner <tanner.johnson2718@gmail.com>
2024-01-14 20:33:33 -0900

    ..

--- components/mac_logger/mac_logger.c
+++ components/mac_logger/mac_logger.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include <errno.h>
 
 #include "mac_logger.h"
 #include "freertos/FreeRTOS.h"
@@ -13,6 +14,8 @@ static uint8_t ap_list_len = 0;
 static uint8_t one_time_init_done = 0;
 
 static const char* TAG = "MAC LOGGER";
+
+#define MOUNT_PATH "/spiffs"
 
 //*****************************************************************************
 // Lock Helpers
@@ -276,7 +279,44 @@ static inline int8_t get_eapol_index(uint8_t* p, wifi_pkt_rx_ctrl_t* rx_ctrl)
 // Handling Eapol PKTS
 //*****************************************************************************
 
+static void eapol_dump_to_disk(uint8_t ap_index)
+{
+    char path[MAX_SSID_LEN];
 
+    snprintf(path, MAX_SSID_LEN, "%s/%.21s.pkt", MOUNT_PATH, ap_list[ap_index].ssid);
+    
+    ESP_LOGI(TAG, "Opening %s to writeout eapol pkts", path);
+    FILE* f = fopen(path, "w");
+    if(!f)
+    {
+        ESP_LOGE(TAG, "Failed to open %s - %s", path, strerror(errno));
+        fclose(f);
+        return;
+    }
+
+    size_t num_written = fwrite(ap_list[ap_index].eapol_pkt_lens, 1, 2*EAPOL_NUM_PKTS, f);
+    if(num_written != 2 * EAPOL_NUM_PKTS)
+    {
+        ESP_LOGE(TAG, "Failed to write EAPOL Header (%d / %d)", num_written, 2*EAPOL_NUM_PKTS);
+        fclose(f);
+        return;
+    }
+
+    uint8_t i;
+    for(i = 0; i < EAPOL_NUM_PKTS; ++i)
+    {
+        num_written = fwrite(ap_list[ap_index].eapol_buffer + i*EAPOL_MAX_PKT_LEN, 1, ap_list[ap_index].eapol_pkt_lens[i], f);
+        if(num_written != ap_list[ap_index].eapol_pkt_lens[i])
+        {
+            ESP_LOGE(TAG, "Failed to write EAPOL %d Pkt (%d / %d)", i, num_written, ap_list[ap_index].eapol_pkt_lens[i]);
+            fclose(f);
+            return;
+        }
+    }
+    
+    fclose(f);
+    ESP_LOGI(TAG, "Write out of EAPOL pkts successful!");
+}
 
 //*****************************************************************************
 // Parse Inbound Packets
@@ -362,6 +402,18 @@ void parse_eapol_pkt(uint8_t eapol_index, uint8_t* p, wifi_pkt_rx_ctrl_t* rx_ctr
     memcpy(ap_list[ap_index].eapol_buffer + (eapol_index*EAPOL_MAX_PKT_LEN), rx_ctrl->sig_len);
     ap_list[ap_index].eapol_pkt_lens[eapol_index] = len;
     ESP_LOGI(TAG, "%s -> Eapol Captured (%d/6)", ap_list[ap_index].ssid, eapol_index);
+
+    uint8_t i;
+    for(i = 0; i < EAPOL_NUM_PKTS; ++i)
+    {
+        if(ap_list[ap_index].eapol_pkt_lens[i] == 0)
+        {
+            _release_lock();
+            return;
+        }
+    }
+
+    eapol_dump_to_disk(ap_index);
 
     _release_lock();
     return;
